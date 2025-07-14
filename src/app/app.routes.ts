ec5e74ec2ffddd851e3c6223c90dc769e0ef4404
Winston5691 <winslangt4@gmail.com>
2025-07-14 16:25:14 +0300

    refactor: remove commented code in app.routes.ts for clarity style: clean up SCSS by removing unnecessary comment in sky-spinner styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,6 @@ export const routes: Routes = [
     path: '',
     component: Layout,
     children: [
-      // specific first
       {
         path: 'solutions/:id',
         loadComponent: () =>
