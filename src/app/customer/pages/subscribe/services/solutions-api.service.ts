34b999eed58a2db72d5072c01a6ba52c9ebad704
Winston Langat <winslangt4@gmail.com>
2025-11-12 07:27:31 +0300

    dort environmental issues

--- src/app/customer/pages/subscribe/services/solutions-api.service.ts
+++ src/app/customer/pages/subscribe/services/solutions-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../../../environments/environment';
+import { environment } from '../../../../../environments/environment';
 
 export interface Solution {
     name: string;
